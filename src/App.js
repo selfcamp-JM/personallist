26f9d11b2b9ad341e3d7174eb7c498b8a1b02fad
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-01-14 02:51:29 +0300

    Persons component changed

--- src/App.js
+++ src/App.js
@@ -7,16 +7,45 @@ const App = ()=>(<Personlist/>)
 
 
 const Personlist=()=>{
+  const people = [
+    {
+      img:22,name:"Jon",job:"Developer"
+    },
+    {
+      img:26,name:"Smith",job:"Student"
+    },
+    {
+      img:23,name:"Link",job:"Teacher"
+    }
+  ]
   return(
-    <Person/>
+  <div>
+    <Person person ={people[0]}/>
+    <Person person ={people[1]}>
+    Hello it is me
+    </Person>
+    <Person person ={people[2]}/>
+  </div>
   )
 }
 
 
-const Person=()=>{
-  const url=https://randomuser.me/api/portraits/thumb/men/20.jpg"
+const Person=(props)=>{
+  const{img,name,job} = props.person;
+  const{children} = props;
+  
+  const url = 'https://randomuser.me/api/portraits/thumb/men/'+img+'.jpg';
+  
   return(
-    <div>I am a person</div>
+    <div className="person">
+    <img src={url} alt=""/>
+    <div>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      <h4>{children}</h4>
+
+    </div>
+    </div>
   )
 }
 
