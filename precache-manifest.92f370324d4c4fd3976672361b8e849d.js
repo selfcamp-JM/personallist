23765283e4fb56bae3d2bb222f9a726c8621ac38
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-01-14 03:21:19 +0300

    Updates

---
+++ precache-manifest.92f370324d4c4fd3976672361b8e849d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ef45ff50b26a6ce08af",
+    "url": "/personallist/static/js/runtime~main.4ef45ff5.js"
+  },
+  {
+    "revision": "3fcbded676f6009763ed",
+    "url": "/personallist/static/js/main.3fcbded6.chunk.js"
+  },
+  {
+    "revision": "ac5061da54c65d4799c1",
+    "url": "/personallist/static/js/1.ac5061da.chunk.js"
+  },
+  {
+    "revision": "3fcbded676f6009763ed",
+    "url": "/personallist/static/css/main.d502e175.chunk.css"
+  },
+  {
+    "revision": "d2f250e6aed8e77c4eb7071d26a02aa3",
+    "url": "/personallist/index.html"
+  }
+];
